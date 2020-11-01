9cad78113f30eb24e05c4ecdead9aa0b985e88fe
sam p <spoveda545@gmail.com>
2020-11-01 18:55:47 -0500

    config.js set

--- src/config.js
+++ src/config.js
@@ -1,4 +1,11 @@
 const PORT = process.env.PORT || 8000;
 const NODE_ENV = process.env.NODE_ENV || production;
 
-module.exports = { PORT, NODE_ENV }
+module.exports = {
+	PORT : process.PORT,
+	NODE_ENV: process.NODE_ENV,
+	TEST_DB_URL: process.TEST_DB_URL,
+	DB_URL: process.DB_URL
+}
+
+// TODO: remove test url export in production
