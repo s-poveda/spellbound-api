30a7638df3ccf4a11ccef9d539d85429f1e35726
s-poveda <spoveda545@gmail.com>
2020-11-02 11:33:49 -0500

    fixed syntax in seed

--- seeds/seed.spells.sql
+++ seeds/seed.spells.sql
@@ -1,5 +1,5 @@
 INSERT INTO spells (title, description)
 VALUES
-('big explosion', 'everyone gets dead.')
-('ineffective death ray', 'anyone killed by this thing gets brought back to life.')
+('big explosion', 'everyone gets dead.'),
+('ineffective death ray', 'anyone killed by this thing gets brought back to life.'),
 ('glove of vengeance', 'If you''re slapped with this item, you must enter a slap battle to the death');
