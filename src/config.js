72e7f3ed78c84c86fcb10db8a937b8de7e91a40a
s-poveda <spoveda545@gmail.com>
2020-11-03 15:16:10 -0500

    fixed config

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 module.exports = {
-	PORT : process.PORT,
-	NODE_ENV: process.NODE_ENV,
-	TEST_DB_URL: process.TEST_DB_URL,
-	DATABASE_URL: process.DB_URL,
-	SPELLBOUND_ADMIN: process.SPELLBOUND_ADMIN,
-	SPELLBOUND_ADMIN_PWD: process.SPELLBOUND_ADMIN_PWD
+	PORT : process.env.PORT,
+	NODE_ENV: process.env.NODE_ENV,
+	TEST_DB_URL: process.env.TEST_DB_URL,
+	DATABASE_URL: process.env.DB_URL,
+	SPELLBOUND_ADMIN: process.env.SPELLBOUND_ADMIN,
+	SPELLBOUND_ADMIN_PWD: process.env.SPELLBOUND_ADMIN_PWD
 }
 
 // TODO: remove test url export in production
