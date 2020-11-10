11f8df952c9dd26c6b7eade93071fc50ae5d834a
sam p <spoveda545@gmail.com>
2020-11-09 20:20:46 -0500

    updated password seeds to hashed

--- seeds/seed.spellbound_test.sql
+++ seeds/seed.spellbound_test.sql
@@ -1,8 +1,13 @@
+BEGIN;
+INSERT INTO users (id, username, password)
+VALUES (0, 'marked for deletion', 'change-in-production');
+COMMIT;
+
 INSERT INTO users (username, password)
 VALUES
-('TheLegend27', 'testpwd001'),
-('G3tSh3r3kd', 'testpwd002'),
-('someDudeWithNoLife', 'testpwd003');
+('TheLegend27', '$2b$16$kNkLNfR8Sn3BG8RHwc3SxO07/N55ahMBnGJtLxzx3GteoVLJflSQ.'),
+('G3tSh3r3kd', '$2b$16$0vbtYJhme0xbnsBcaK3dgOVsbexP9/moZkaXGE17iNbzv4F3cTo0i'),
+('someDudeWithNoLife', '$2b$16$gF/ax8LUYnfoDxIP02xNrOIxty4YOgUjw/mCz7pppbNUczZfTRWKG');
 
 INSERT INTO spells (title, description)
 VALUES
