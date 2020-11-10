6fbda8a7b457c282f4d252a6b9c6e9b13ce76ebf
sam p <spoveda545@gmail.com>
2020-11-09 20:21:37 -0500

    requireAuth middleware first pass

---
+++ src/middleware/requireAuth.js
@@ -0,0 +1,17 @@
+const AuthService = require('../AuthService');
+
+function requireAuth(req, res, next) {
+	const authToken = req.get('Authorization') || '';
+	let bearerToken = null;
+	if (!authToken.toLowerCase().startsWith('bearer ')) {
+		return res.status(401).json({ error: 'Missing bearer token' });
+	} else {
+		bearerToken = authToken.slice(7, authToken.length);
+	}
+	try {
+		AuthService.verifyJwt(bearerToken);
+		next();
+	} catch (error) {
+		res.status(401).json({ error: 'Unauthorized request' });
+	}
+}
