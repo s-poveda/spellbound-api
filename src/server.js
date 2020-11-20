25d4bea4fa41c21dcf140b9164ead9c00f8b2541
sam p <spoveda545@gmail.com>
2020-11-20 18:25:08 -0500

    fixed db url refence

--- src/server.js
+++ src/server.js
@@ -1,16 +1,11 @@
 require('dotenv').config();
 const knex = require('knex');
 const app = require('./app.js');
-const { PORT, SPELLBOUND_ADMIN, SPELLBOUND_ADMIN_PWD } = require('./config');
+const { PORT, DATABASE_URL } = require('./config');
 
 app.set('db', knex({
 	client: 'pg',
-	connection: {
-		host: 'localhost',
-		user: SPELLBOUND_ADMIN,
-		password: SPELLBOUND_ADMIN_PWD,
-		database: 'spellbound',
-	}
+	connection: DATABASE_URL,
 }));
 
 app.listen(PORT, ()=> console.log(`Listening on port ${PORT}`));
